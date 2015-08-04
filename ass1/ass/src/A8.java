2a5cc898de23ed63e33e4913f1c7eb1091967280
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-04 19:28:58 +0800

    V2.0

---
+++ ass1/ass/src/A8.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class A8 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("請輸入A值");
+		Float A = scn.nextFloat();
+		System.out.print("請輸入B值");
+		Float B = scn.nextFloat();
+		if(A>B){System.out.print(A+">"+B);}
+		
+		else if(A<B){System.out.print(A+"<"+B);}
+		
+		else{System.out.print(A+"="+B);}
+			
+		
+		
+
+		
+		
+		
+		
+		
+
+	}
+
+}
