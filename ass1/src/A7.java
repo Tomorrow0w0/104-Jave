e9834127276d586299413e61d1f8f70ac7e20b5a
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-04 11:57:00 +0800

    華氏攝氏

---
+++ ass1/src/A7.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class A7 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn =new Scanner(System.in);
+		System.out.println("請輸入攝氏");
+		Float C = scn.nextFloat();
+		Float F = C*9/5+32;
+		System.out.print("華氏為"+F);
+				
+		
+		
+		
+		
+		
+
+	}
+
+}
