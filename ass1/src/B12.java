9fe905a17be34d6a4b5b60fdca63bde378a067b9
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-05 13:04:08 +0800

    v3.0

---
+++ ass1/src/B12.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class B12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner scn = new Scanner(System.in);
+        System.out.print("請輸入國文成績");
+		int Ch = scn.nextInt();
+		System.out.print("請輸入英文成績");
+		int E = scn.nextInt();
+		System.out.print("請輸入數學成績");
+		int C = scn.nextInt();
+		
+		int End=Ch+E+C;
+		int End2=(Ch+E+C)/3;
+		
+		System.out.print("總成績為"+End);
+		System.out.print("每科平均"+End2);
+		
+		
+     
+     
+     
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
