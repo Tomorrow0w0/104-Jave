3e56317360ffceb28de51ca3b7865f32b4cf0d44
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-05 11:52:26 +0800

    v3.0

---
+++ ass1/src/B13.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class B13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int enternumber;
+		Scanner scn = new Scanner(System.in);
+
+        System.out.println("叫块タ俱计耞ㄤ案");
+		
+		enternumber = scn.nextInt();
+
+		if(enternumber%2==0){
+
+		System.out.printf("%d案计",enternumber);
+
+		}else{ System.out.printf("%d计",enternumber);}
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
