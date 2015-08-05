1015e6d4e99c6818bfdb4086ec80d1834fbb9626
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-05 13:21:24 +0800

    v3.0

---
+++ ass1/src/B11.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class B11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入整數A=");
+		int A = scn.nextInt();
+		System.out.print("請輸入整數B=");
+		int B = scn.nextInt();
+		
+		int End  = A+B;
+		int End1 = A-B;
+		int End2 = A*B;
+		
+		System.out.println("兩數合為"+End);
+		System.out.println("兩數差為"+End1);
+		System.out.println("兩數乘積為"+End2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
