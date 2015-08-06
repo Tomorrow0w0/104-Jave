863fa96912fd52b372f7d7cd7ce9639e87c16ca1
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-06 11:46:28 +0800

    判斷三角形

---
+++ ass1/src/B6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class B6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+		
+		System.out.println("請輸入三角形邊長a= ");
+		Float a = scn.nextFloat();
+		System.out.println("請輸入三角形邊長b= ");
+		Float b = scn.nextFloat();
+		System.out.println("請輸入三角形邊長c= ");
+		Float c = scn.nextFloat();
+		
+	if(a<=b && b<=c){	
+		
+		if(a*a+b*b < c*c){System.out.println(" 鈍角三角形");}
+		if(a*a+b*b == c*c){System.out.println("直角三角形");}
+		if(a*a+b*b > c*c){System.out.println(" 銳角三角形");}
+		
+	}else{System.out.println("無此三角形");}
+		
+	}
+
+}
