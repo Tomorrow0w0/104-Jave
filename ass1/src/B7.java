6ba34ec0d241cb3740a3e732070e4208169fba6f
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-06 13:14:43 +0800

    學期

---
+++ ass1/src/B7.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class B7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入上學期subject1的成績");
+		int sj1 = scn.nextInt();
+		System.out.println("請輸入上學期subject2的成績");
+		int sj2 = scn.nextInt();
+		System.out.println("請輸入上學期subject3的成績");
+		int sj3 = scn.nextInt();
+		System.out.println("請輸入上學期subject4的成績");
+		int sj4 = scn.nextInt();
+		System.out.println("請輸入上學期subject5的成績");
+		int sj5 = scn.nextInt();
+		System.out.println("請輸入上學期subject6的成績");
+		int sj6 = scn.nextInt();
+		
+		System.out.println("請輸入下學期subject1的成績");
+		int sj7 = scn.nextInt();
+		System.out.println("請輸入下學期subject2的成績");
+		int sj8 = scn.nextInt();
+		System.out.println("請輸入下學期subject3的成績");
+		int sj9 = scn.nextInt();
+		System.out.println("請輸入下學期subject4的成績");
+		int sj10 = scn.nextInt();
+		System.out.println("請輸入下學期subject5的成績");
+		int sj11 = scn.nextInt();
+		
+		if(sj1>=60){}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
