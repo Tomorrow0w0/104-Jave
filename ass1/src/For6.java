de60bdbf5e5da58063a4ec52114a0a96ab0727f1
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-10 12:38:44 +0800

    3.0

---
+++ ass1/src/For6.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class For6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤Jn­È");
+		int n = scn.nextInt();
+
+		for(int i=0;i<n;i=i+1){
+			for(int j=0;j<=i;j=j+1){
+			
+				System.out.print("*");
+			 
+		   }
+			    System.out.println();    
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ ass1/src/For7.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class For7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤Jn­È");
+		
+		int n = scn.nextInt();
+		
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i-1;j++){
+				System.out.print(" ");
+					
+			}
+		
+					
+		     for(int k=0;k<=i*2;k++){
+			    System.out.print("*");
+		   }		
+					
+					
+		       System.out.println("");
+					
+		    }			
+					
+					
+			       
+		}
+		
+}	
+		
+	
+
+
