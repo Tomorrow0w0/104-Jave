c8e584ee5ef2db7c6bda189fa839869ef6e940a7
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-11 13:05:22 +0800

    v3.0

---
+++ ass1/src/Array1.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+        Random rnd = new Random();
+ 
+ 
+
+          int date[]  = new int[10];   
+          int  count=0;
+		for(int i=0;i<10;i++){
+			date[i]=rnd.nextInt(100);
+			if(date[i]%2==0){
+			count++;	
+		     }
+		}
+		
+		 
+		int	j = 1;
+        while(j<=3){
+        	System.out.println("請猜有多少個偶數");
+           
+          int k=scn.nextInt();
+         if(k==count){
+        		System.out.println("恭喜你猜對了");
+              break;
+         
+         }else{
+        	 
+        	 System.out.println("你輸了");}	
+         
+              j++;
+        
+        }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+		
+	}
+
+}
---
+++ ass1/src/While2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class While2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入年份");
+		int n = scn.nextInt();
+		 
+		while(n%4!=0){
+			
+			
+			System.out.print(n+"不是閏年");
+			break;
+	   }
+		
+		
+		    System.out.print(n+"是閏年");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
