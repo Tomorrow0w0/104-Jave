e5024db9eda13680dfe984e6911d503238038477
Tomorrow0w0 <patapon1245rv@gmail.com>
2015-08-17 14:59:22 +0800

    最大公因數,其他寫法

--- ass1/src/Date8017d2.java
+++ ass1/src/Date8017d2.java
@@ -4,28 +4,23 @@ public class Date8017d2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		int X,Y,Z;
+		int X,Y;
 		 System.out.print("請輸入X= ");
 		   X = scn.nextInt();
 		  while(X!=999){
 			  System.out.printf("請輸入Y= ");
 			   Y = scn.nextInt();
-			   System.out.printf("請輸入Z= ");
-			   Z = scn.nextInt();
-			  System.out.println("最大公因數為 = " + factorial(X,Y,Z));
-             break;
+			  System.out.println("最大公因數為 = " + gcd(X,Y));
+            break;
 		  }
 	     }
-	
-	   
-	    static int factorial(int X,int Y,int Z){
-	     if (Y == 0) return X;
-	     else return factorial(Y, X % Y, Z);
 
-       }
+	   static int gcd(int X,int Y){
+   	int R=X%Y;
+	     if (R == 0) return Y;
+	     else return gcd(Y, R);
 
-
-
-	}
+	   }
+	   }
 
 
